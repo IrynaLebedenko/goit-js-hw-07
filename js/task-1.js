39b38a2d1b4726be58356193db70764b810ad2dd
IrynaLebedenko <94801312+IrynaLebedenko@users.noreply.github.com>
2024-01-21 21:53:25 +0200

    continue create hw-7

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,16 @@
 const categoriesList = document.getElementById("categories");
+
 const categoriesItems = categoriesList.querySelectorAll("li.item");
 
 console.log(`Total categories: ${categoriesItems.length}`);
 
 categoriesItems.forEach(category => {
   const categoryTitle = category.querySelector("h2").textContent;
-  const categoryItemsCount = category.querySelectorAll("li").length;
-  console.log(`Category: ${categoryTitle}`);
-  console.log(`Items count: ${categoryItemsCount}`);
-});
+  const subItems = categoryItem.querySelectorAll("ul li");
 
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Number of items: ${subItems.length}`);
+});
 
 
 
